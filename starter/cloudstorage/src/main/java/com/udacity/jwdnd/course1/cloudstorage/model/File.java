75a92999008e50e7510fcd45634e265925081534
eunhiekim <eunhiekim@gmail.com>
2021-01-17 14:00:07 -0500

    adding and connecting controllers

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -0,0 +1,61 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+
+public class File {
+
+    private Integer fieldId;
+    private String fileName;
+    private String contentType; // or enum
+    private String fileSize;
+    private Integer userId;
+    private byte[] fileData;
+
+    public File(Integer fieldId, String fileName, String contentType, String fileSize, Integer userId, byte[] fileData) {
+        this.fieldId = fieldId;
+        this.fileName = fileName;
+        this.contentType = contentType;
+        this.fileSize = fileSize;
+        this.userId = userId;
+        this.fileData = fileData;
+    }
+
+    public Integer getFieldId() {
+        return fieldId;
+    }
+
+    public void setFieldId(Integer fieldId) {
+        this.fieldId = fieldId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public byte[] getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(byte[] fileData) {
+        this.fileData = fileData;
+    }
+}
